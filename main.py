21bd76e5ab9373d295851f33c3b6bd9e91038bf2
Bhavik-punmiya <118907757+Bhavik-punmiya@users.noreply.github.com>
2024-03-29 11:03:39 +0530

    model_training_implemented

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from cnnClassifier import logger
 from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
-from cnnClassifier.pipeline.stage02_prepare_base_model import PrepareBaseModelTrainingPipeline
-
+from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from cnnClassifier.pipeline.stage_03_model_training import ModelTrainingPipeline
 
 STAGE_NAME = "Data Ingestion Stage"
 
@@ -20,6 +20,17 @@ try:
     logger.info(f"Starting {STAGE_NAME}")
     prepare_base_model = PrepareBaseModelTrainingPipeline()
     prepare_base_model.main()
+    logger.info(f"Completed {STAGE_NAME}")
+except Exception as e:
+    logger.error(f"Failed to execute {STAGE_NAME} due to error {e}")
+    raise e
+
+STAGE_NAME = "Model Training Stage"
+
+try:
+    logger.info(f"Starting {STAGE_NAME}")
+    model_training = ModelTrainingPipeline()
+    model_training.main()
     logger.info(f"Completed {STAGE_NAME}")
 except Exception as e:
     logger.error(f"Failed to execute {STAGE_NAME} due to error {e}")
